79328acca49b5afcb916915728a499307764282f
piyasnhu31 <piyanshukale2005@gmail.com>
2025-09-28 16:39:13 +0530

    dsfdsd

--- app/src/main/java/com/example/vlctracker/MainActivity.java
+++ app/src/main/java/com/example/vlctracker/MainActivity.java
@@ -1,26 +1,12 @@
 package com.example.vlctracker;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView timerTextView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timerTextView = findViewById(R.id.timerTextView);
-
-        // Display the current listening time
-        timerTextView.setText("Listening time: " + MediaListenerService.getListeningTime() + " seconds");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Optionally, update the timer every second
     }
 }
--- app/src/main/java/com/example/vlctracker/MediaListenerService.java
+++ app/src/main/java/com/example/vlctracker/MediaListenerService.java
@@ -1,42 +1,36 @@
 package com.example.vlctracker;
 
+import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
-import android.os.Handler;
+import android.app.Notification;
+import android.util.Log;
 
 public class MediaListenerService extends NotificationListenerService {
 
-    private static int listeningTime = 0; // in seconds
-    private static boolean isPlaying = false;
-    private Handler handler = new Handler();
-
-    private Runnable timerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            if (isPlaying) {
-                listeningTime++;
-                handler.postDelayed(this, 1000); // count every second
-            }
-        }
-    };
-
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        if (sbn.getPackageName().equals("org.videolan.vlc")) {
-            isPlaying = true;
-            handler.postDelayed(timerRunnable, 1000);
+        String pkg = sbn.getPackageName();
+
+        // Log every notification
+        Log.d("VlcTracker", "Notification from: " + pkg);
+
+        if (pkg.equals("org.videolan.vlc")) {
+            Log.d("VlcTracker", "🎵 VLC notification detected!");
+
+            Notification notification = sbn.getNotification();
+            if (notification != null) {
+                Bundle extras = notification.extras;
+                String title = extras.getString(Notification.EXTRA_TITLE);
+                String text = extras.getString(Notification.EXTRA_TEXT);
+
+                Log.d("VlcTracker", "Now playing: " + title + " - " + text);
+            }
         }
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
-        if (sbn.getPackageName().equals("org.videolan.vlc")) {
-            isPlaying = false;
-            handler.removeCallbacks(timerRunnable);
-        }
-    }
-
-    public static int getListeningTime() {
-        return listeningTime;
+        Log.d("VlcTracker", "Notification removed: " + sbn.getPackageName());
     }
 }
