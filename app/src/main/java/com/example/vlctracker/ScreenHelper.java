b56196542487a7828f51d80fafc2a8870595a945
piyasnhu31 <piyanshukale2005@gmail.com>
2026-01-13 18:30:04 +0530

    start again

--- app/src/main/java/com/example/vlctracker/ScreenHelper.java
+++
@@ -1,8 +0,0 @@
-package com.vlctracker;
-
-public class ScreenHelper {
-    public static boolean isScreenActive() {
-        // TODO: Replace with real check (for now assume screen is on)
-        return true;
-    }
-}
